b90ff1233fdc0794e4f41c38064f9192071f689f
unknown <alex.delaitre123@gmail.com>
2022-04-18 22:51:26 +0100

    awesome adding function!!!

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -1,26 +1,8 @@
+fn plus_one(a: i32) -> i32 {
+    a + 1
+    // There is no ending ; in the above line.
+    // It means this is an expression which equals to `return a + 1;`.
+}
 fn main() {
-    let a; // Declaration; without data type
-    a = 5; // Assignment
-
-    let b: i8; // Declaration; with data type 
-    b = 5;
-
-    let t = true;        // Declaration + assignment; without data type
-    let f: bool = false; // Declaration + assignment; with data type
-
-    let (x, y) = (1, 2); // x = 1 and y = 2
-
-    let mut z = 5;
-    z = 6;
-
-    let z = { x + y }; // z = 3
-
-    let z = {
-        let x = 1;
-        let y = 2;
-
-        x + y
-    }; // z = 3
-    println!("{}",z);
-
+    println!("{}",plus_one(3));
 }
